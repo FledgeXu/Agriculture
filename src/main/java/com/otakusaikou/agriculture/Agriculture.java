86f15dec241b6f555cbbcca3beafe4aa67339885
FledgeXu <xzk0701@gmail.com>
2020-12-06 16:57:28 +0800

    Init.

---
+++ src/main/java/com/otakusaikou/agriculture/Agriculture.java
@@ -0,0 +1,8 @@
+package com.otakusaikou.agriculture;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Agriculture.MOD_ID)
+public class Agriculture {
+    public static final String MOD_ID = "agriculture";
+}
